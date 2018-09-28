00614665bf5dae6a7dd9099f2275b1ea486b964b
Kale-Eos <pacoellaga@gmail.com>
2018-09-27 18:51:25 -0700

    init proj commit

---
+++ Squire (UI)/Assets/Script/TempGoBack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempGoBack : MonoBehaviour {
+
+    public void BackToMainMenu ()
+    {
+        // Reverses the the scene change to go back one scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
