30208b99d8125cc1e48df061800f24e2d4f57a74
Kale-Eos <pacoellaga@gmail.com>
2018-10-05 14:50:42 -0700

    stuff unfinished

---
+++ Squire (UI)/Assets/Script/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            gameObject.AddComponent<AudioSource>();
+            gameObject.
+        }
+        
+    }
+
+}
---
+++ Squire (UI)/Assets/Script/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audiomixer;
+
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("Volume", volume);
+        Debug.Log(volume);
+    }
+
+}
