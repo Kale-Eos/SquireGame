5779dc86823e1e884bb219f5ec8e71924a10c426
Kale-Eos <pacoellaga@gmail.com>
2018-10-14 22:49:18 -0700

    committing changes to pull

--- Squire (UI)/Assets/Script/MainMenu.cs
+++ Squire (UI)/Assets/Script/MainMenu.cs
@@ -5,34 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    [SerializeField]                                        // instantiates field
+    string hoverOverSound = "ButtonHover";                  // instantiates Hover Over Sound
+    [SerializeField]                                        // instantiates field
+    string pressButtonSound = "ButtonPress";                // instantiates Press Button Sound
+
+    AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = AudioManager.instance;               // instantiates AudioManager
+        if (audioManager == null)
+        {
+            Debug.LogError("No AudioManager Found");
+        }
+    }
+
+    public void OnMouseOver()
+    {
+        audioManager.PlaySound(hoverOverSound);             // all buttons makes a sound when hovering
+    }
+
+    public void OnMouseDown()
+    {
+        audioManager.PlaySound(hoverOverSound);             // all buttons makes a sound when hovering
+    }
+
     public void PlayTutorial ()
     {
-        // loads onto the tutorial scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);       // loads onto the tutorial scene
     }
 
     public void PlayLevel1()
     {
-        // loads onto the Level 1 scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);       // loads onto the Level 1 scene
     }
 
     public void PlayLevel2()
     {
-        // loads onto the Level 2 scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);       // loads onto the Level 2 scene
     }
 
     public void BackToMainMenu()
     {
-        // Reverses the the scene change to go back to Title Scene
-        SceneManager.LoadScene("Title Scene");
+        SceneManager.LoadScene("Title Scene");      // Reverses the the scene change to go back to Title Scene
     }
 
     public void QuitGame ()
     {
-        // types out Quitting Game on Console and Quits game
-        Debug.Log("Quitting Game");
-        Application.Quit();
+        Debug.Log("Quitting Game");     // types out ~Quitting Game~
+        Application.Quit();             // and quits game
     }
+
 }
