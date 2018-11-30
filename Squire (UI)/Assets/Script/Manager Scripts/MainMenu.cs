085e322139a8b1232335a878422b52e49ff68d3e
Kale-Eos <pacoellaga@gmail.com>
2018-11-29 19:32:01 -0800

    changes made today

--- Squire (UI)/Assets/Script/Manager Scripts/MainMenu.cs
+++ Squire (UI)/Assets/Script/Manager Scripts/MainMenu.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
     [SerializeField]                                        // instantiates field
     string hoverOverSound = "ButtonHover";                  // instantiates Hover Over Sound
