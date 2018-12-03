25f64d5673fea85356ff20d0b207725939e49176
Stephen Long <stephenlong7@gmail.com>
2018-12-02 20:31:33 -0800

    moved the hearts and other stuff over to weston's assets

---
+++ Squire (UI)/Assets/WestsAssets/Canvas_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas_Script : MonoBehaviour {
+
+	public GameObject INGameCanvas2;
+
+	void Start() {
+		/* The in game canvas is not active at first */
+		INGameCanvas2.SetActive (false);
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		/* when esc is pressed, the canvas shows/ disappears depending on whether is was showing before or not */
+		if (Input.GetButtonDown("Pause")) {
+			Debug.Log ("pause");
+			if (INGameCanvas2.activeSelf) {
+				INGameCanvas2.SetActive (false);
+			} 
+			else {
+				INGameCanvas2.SetActive (true);
+			}
+		}
+		
+	}
+}
---
+++ Squire (UI)/Assets/WestsAssets/Health.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public GameObject Squire;
+	/* When the player has one heart left */
+	public GameObject Hearts1;
+	/* When the player has two hearts left */
+	public GameObject Hearts2;
+	/* When the player has three hearts left */
+	public GameObject Hearts3;
+	/* The squire initially has 3 hearts, so he can take 3 hits before he dies */
+	public int Hearts;
+	/* The squire becomes invincible for a while if he takes damage */
+	public bool invincible;
+
+	// Use this for initialization
+	void Start () {
+		Hearts = 3;
+		Hearts3.SetActive (true);
+		invincible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* When the number of hearts equals 0, the squire will die. */
+		if (Hearts == 0) {
+			/* Plays a death animation if there is any and then transitions to death scene */
+		} 
+		/* When the number of hearts equals 1, hearts1 appears */
+		else if (Hearts == 1) {
+			Hearts3.SetActive (false);
+			Hearts2.SetActive (false);
+			Hearts1.SetActive (true);
+		} 
+		/* When the number of hearts equals 2, hearts2 appears */
+		else if (Hearts == 2) {
+			Hearts3.SetActive (false);
+			Hearts2.SetActive (true);
+		} 
+	}
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (!invincible) {
+			/* If the squire touches anything with the tag "harmful" while he isn't invincible, he will take damage */
+			if (collision.gameObject.CompareTag("harmful")) {
+				/* Paco you can add in the animation right here */
+				Hearts = Hearts - 1;
+				invincible = true;
+				/* Invulnerability period ends after 0.5 seconds*/
+				Invoke("notInvincible", 0.5f);
+			}
+		}
+	}
+	void notInvincible() {
+		invincible = false;
+	}
+}
