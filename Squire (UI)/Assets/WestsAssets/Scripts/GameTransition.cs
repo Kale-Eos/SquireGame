df40066ce542b31c1d7f092071b1d3bc98b5c233
Stephen Long <stephenlong7@gmail.com>
2018-12-03 18:37:55 -0800

    Finished fading for everything except pause to quit in tutorial

--- Squire (UI)/Assets/WestsAssets/Scripts/GameTransition.cs
+++ Squire (UI)/Assets/WestsAssets/Scripts/GameTransition.cs
@@ -5,13 +5,22 @@ using UnityEngine.Audio;
 public class GameTransition : MonoBehaviour
 {
     AudioManager audioManager;
+	private LevelChanger2 levelChanger2;
+	public GameObject player;
+
+	void Start() {
+		levelChanger2 = GameObject.Find("LevelChanger").GetComponent<LevelChanger2> ();
+	}
+
 
     // Scene transition when player enters collider.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            SceneManager.LoadScene("Credits Scene");
+			levelChanger2.FadeToLevel (4);
+			player.GetComponent<PlayerControllerV3> ().enabled = false;
+            //SceneManager.LoadScene("Credits Scene");
         }
     }
 }
