6f9564d52ecc6717932e98bc272e469c226c823e
Weston Elliott <Westonini101@gmail.com>
2018-12-06 11:15:30 -0800

    Fixed up the Game Over UI showing up. Now also freezes player when activated.

--- Squire (UI)/Assets/Script/Manager Scripts/GameOver.cs
+++ Squire (UI)/Assets/Script/Manager Scripts/GameOver.cs
@@ -7,30 +7,33 @@ public class GameOver : MonoBehaviour
 {
     AudioManager audioManager;
 
+    private PlayerControllerV3 PCV3;
+
     bool gameEnded = false;
 
     public float delay = 1f;
+    private float delay2 = 0.1f;
+
+    public GameObject player;
 
     public GameObject GameOverUI;
+
 
 	void Start ()
     {
         audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        PCV3 = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerV3>();
+
     }
 
 	public void EndGame()
     {
+
         if (gameEnded == false)                 // checks if game ended
         {
-            gameEnded = true;                   // condition is true
-            Invoke("Retry", delay);
-            
-            // trying to activate GameOverUI gameobject, but i fucked up somewhere
-            //Invoke("GameOverUI", delay);        // waits 1 sec then calls UI
-            //if (GameOverUI == false)
-            //{
-            //    GameOverUI.SetActive(true);
-            //}
+            gameEnded = true;
+            Invoke("ActivateUI", delay);        // Waits 1 second and then turns on the GameOver UI.
+            FreezePlayer();                     // Freezes the player.
         }
     }
 
@@ -39,4 +42,21 @@ public class GameOver : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
         audioManager.PlaySound("Return2Game_UI");
     }
+
+    public void ActivateUI()
+    {
+        GameOverUI.SetActive(true);
+    }
+
+    public void FreezePlayer()
+    {
+        PCV3.movementSpeed = 0;
+        Invoke("PlayerControllerOff", delay2);   //Waits 0.1 seconds and then disables PlayerControllerV3. It's coded this way because otherwise the player would continue sliding.
+    }
+
+    public void PlayerControllerOff()
+    {
+        player.GetComponent<PlayerControllerV3>().enabled = false;
+    }
+
 }
